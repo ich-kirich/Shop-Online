1dc5bc53df4ff432317966fbd488eb27561da953
Kirill Medved <tankobaldoytpa@gmail.com>
2024-03-04 12:34:46 +0300

    feat: add swagger documentation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+    @ApiProperty({example: "Medvedik@gmail.com", description: "User email"})
     readonly email: string;
+    @ApiProperty({example: "StrongPassword", description: "User password"})
     readonly passsword: string;
+    @ApiProperty({example: "Medvedik", description: "User Name"})
+    readonly name: string;
 }
