ec8f8287c428fbb8edc9c385bd448de945dce187
Kirill Medved <tankobaldoytpa@gmail.com>
2024-03-04 14:16:39 +0300

    feat: add registration and login for users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -4,7 +4,7 @@ export class CreateUserDto {
     @ApiProperty({example: "Medvedik@gmail.com", description: "User email"})
     readonly email: string;
     @ApiProperty({example: "StrongPassword", description: "User password"})
-    readonly passsword: string;
+    readonly password: string;
     @ApiProperty({example: "Medvedik", description: "User Name"})
     readonly name: string;
 }
