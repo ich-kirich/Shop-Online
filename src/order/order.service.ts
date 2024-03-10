8d833d5b2831edf3f72fc2d3cd9604200a884cbf
Kirill Medved <tankobaldoytpa@gmail.com>
2024-03-10 14:45:46 +0300

    feat: add model order

---
+++ src/order/order.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { Order } from './order.model';
+
+@Injectable()
+export class OrderService {
+    constructor(@InjectModel(Order) private orderRepository: typeof Order){}
+
+    async create(dto: CreateOrderDto) {
+        const feedback = await this.orderRepository.create(dto);
+        return feedback;
+    }
+
+    async getOrderByUserId(id: number) {
+        const feedback = await this.orderRepository.findOne({
+          where: { id },
+          include: { all: true },
+        });
+        return feedback;
+      }
+}
