86e19e876d78ec2c9c8131bef4ee9e1fdc1ac086
Kirill Medved <tankobaldoytpa@gmail.com>
2024-04-15 13:32:15 +0300

    docs: update readme

--- src/models/auth/dto/create-auth.dto.ts
+++ src/models/auth/dto/create-auth.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNumber, IsString, Length, Max, Min } from "class-validator";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class LoginUserDto {
   @ApiProperty({ example: "Medvedik@gmail.com", description: "User email" })
